49f063d8234c0c1bb61ec2f8e3d639c31773b805
Nurullah Mert GULER <mertg1129@gmail.com>
2022-12-23 10:35:36 +0300

    public enum
    example public enum script

---
+++ Assets/PublicEnums/PublicEnums.cs
@@ -0,0 +1,27 @@
+
+public enum AddSelector
+{
+    Add,
+    Remove
+}
+
+public enum ActiveEnum
+{
+    Active,
+    Inactive
+}
+
+public enum ClueEnum
+{
+    Magnet,
+    MagnifyingGlass
+}
+
+public enum SoundEnum
+{
+    gift,
+    itemcollect,
+    itemticked,
+    buttonDown,
+    levelcomplete
+}
